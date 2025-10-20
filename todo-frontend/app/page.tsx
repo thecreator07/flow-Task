9085aa5cec0f92785d10bf99aa3c8263b99dde20
Aman Kumar Prasad <amankumarprasad72@gmail.com>
2025-10-20 22:00:21 +0530

    some little changes in dockers

--- todo-frontend/app/page.tsx
+++ todo-frontend/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    if (!loading) {
+    if (!loading && user===null) {
       if (user) {
         router.push('/dashboard');
       } else {
